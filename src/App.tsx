548c3823a2609ef930a30c31ec0b14c4a9e1807a
CJM1994 <connorjohnmullin@gmail.com>
2022-06-19 15:26:13 -0700

    create formik form

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,39 @@
+// Emotion
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import { jsx } from "@emotion/react";
+import { css } from "@emotion/react";
+
+// Material UI
 import { Container, Grid } from "@mui/material";
 import Header from "./components/Header";
+
+// Formik
+import { Formik, Form } from "formik";
+
+// Yup
+import * as Yup from "yup";
+
+// CSS
 import "./styles/App.css";
+
+const styles = {
+  header: css({
+    backgroundColor: "red",
+    marginTop: "100px",
+  }),
+};
+
+const INITIAL_FORM_STATE = {};
+
+const VALIDATION_SCHEMA = {};
 
 const App = () => {
   return (
     <Grid container>
       {/* A grid container holds grid items */}
       <Grid item xs={12}>
-      {/* Breakpoints like xs sm md lg xl are like @media queries in css and
+        {/* Breakpoints like xs sm md lg xl are like @media queries in css and
        determine the number of columns taken up at different screen sizes, 
       takes values from 1-12 with 12 being the full screen width */}
         <Header />
@@ -15,7 +41,15 @@ const App = () => {
       <Grid item xs={12}>
         <Container maxWidth={"md"}>
           {/* A container centers your content horizontally */}
-          <div></div>
+          <div css={styles.header}>
+            <Formik
+              initialValues={INITIAL_FORM_STATE}
+              onSubmit={() => console.log("Submit")}
+              validationSchema={VALIDATION_SCHEMA}
+            >
+              <Form></Form>
+            </Formik>
+          </div>
         </Container>
       </Grid>
     </Grid>
