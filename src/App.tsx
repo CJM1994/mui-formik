ba9aac202c6a960cd3f0a3ee9cdef33ebfd04193
CJM1994 <connorjohnmullin@gmail.com>
2022-06-18 18:26:42 -0700

    add grids and container for main page layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
+import { Container, Grid } from "@mui/material";
 import Header from "./components/Header";
+import "./styles/App.css";
 
 const App = () => {
-  return <Header />;
+  return (
+    <Grid container>
+      {/* A grid container holds grid items */}
+      <Grid item xs={12}>
+      {/* Breakpoints like xs sm md lg xl are like @media queries in css and
+       determine the number of columns taken up at different screen sizes, 
+      takes values from 1-12 with 12 being the full screen width */}
+        <Header />
+      </Grid>
+      <Grid item xs={12}>
+        <Container maxWidth={"md"}>
+          {/* A container centers your content horizontally */}
+          <div></div>
+        </Container>
+      </Grid>
+    </Grid>
+  );
 };
 
 export default App;
